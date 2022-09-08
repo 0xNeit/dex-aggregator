e816f48c78c270ff9cf4522c5c857662c00f8a79
CryptoNate <89227631+DeFiDruid@users.noreply.github.com>
2022-09-08 02:59:26 +0100

    added css styles and connect button

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,54 @@
-// Navigaton bar component
+// import { useState } from "react";
 import styled from "styled-components";
+import Logo from "../assets/eth-logo.png";
 
 const Nav = styled.div`
     width: 100%;
     height: 80px;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
     padding: 0 max(calc(50vw - 550px), 20px);
-    `;
+    `
+
+const ConnectButton = styled.button`
+    font-size: 1.1rem;
+    background-color: var(--light);
+    border: 1px solid var(--background);
+    bordcer-radius: 8px;
+    padding: 8px 36px;
+    margin-left: auto;
+    &:hover {
+        border: 1px solid var(--light-dark);
+    }
+    `
+
+const StyledIcon = styled.img`
+    width: 40px;
+    height: 40px;
+    object-fit: contain;
+    margin-right: 12px;
+    `
+
+const StyledTitle = styled.div`
+    font-size: 1.1rem;
+    font-weight: bold;
+    `
+
+    
+const WalletManager = () => {
+    return (
+        <ConnectButton>Connect Wallet</ConnectButton>   
+        )
+    }
 
 const NavBar = () => (
-    <Nav></Nav>
+    <Nav>
+        <StyledIcon src={Logo} />
+        <StyledTitle>Cross DEX</StyledTitle>
+        <WalletManager></WalletManager>
+    </Nav>
 )
 
 // Layout component
